0fffe56951e5cdff6722c09080c2854f04a451d5
RaquelLima7 <raquellimadev@gmail.com>
2023-01-30 21:43:49 -0300

    Model contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,19 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  # has_many :phones
+  # has_one :address
+
+  validates :full_name, presence: true
+  validates :document_number, uniqueness: { scope: :user_id }
+  validates :email, presence: true, uniqueness: { scope: :user_id }, format: { with: /\A([^\s]?[\w\-+.\/"\%\!]+)@([A-Za-z\d\-\[\]\.\:\(\)]+)\z/i }
+
+  validate :birthday_date, on: %i[create update], if: :birthday
+
+  private
+
+  def birthday_date
+    return unless (birthday > Time.now - 18.years) && (birthday > Time.now - 100.years)
+
+    errors.add(:birthday, ' has to be a date greater than 18 years and less than 100 years.')
+  end
 end
